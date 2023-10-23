39a83decc613375d8db7ecccb6f24a6a46b458be
tuwatcher <tuwatcher@gmail.com>
2023-10-23 13:22:50 +0300

    Production code without tests;

--- main.cpp
+++ main.cpp
@@ -2,42 +2,20 @@
 #include <string>
 #include <array>
 
-#include "googletest/googletest/include/gtest/gtest.h"
-#include "googletest/googlemock/include/gmock/gmock.h"
 
-std::string ConvertArabic(unsigned int number);
 struct ArabicToRomanMap
 {
     unsigned int arabicNumber;
-    std::string romanNumber;
-};
-class RomanNumeralAssert
-{
-    public:
-
-        RomanNumeralAssert() = delete;
-        explicit RomanNumeralAssert(const unsigned int arabicNumeral) :
-            arabicNumberToConvert(arabicNumeral) {}
-        void isConvertedToRomanNumeral(const std::string& ExpectedRomanNumeral) const
-        {
-            ASSERT_EQ(ExpectedRomanNumeral, ConvertArabic(arabicNumberToConvert));
-        }
-    
-    private:
-        const unsigned int arabicNumberToConvert;
+    std::string romanNumber;    
 };
 
-RomanNumeralAssert assertThat(const unsigned int arabicNumber)
-{
-    RomanNumeralAssert assert { arabicNumber };
-    return assert;
-}
+std::string ConvertArabic(unsigned int number);
 
-int main(int argc, char **argv)
+int main()
 {
-    testing::InitGoogleTest(&argc, argv);
-   // std::cout << ConvertArabic(23);
-    return RUN_ALL_TESTS();
+    std::cout << ConvertArabic(564);
+
+    return 0;
 }
 
 std::string ConvertArabic(unsigned int number)
@@ -73,26 +51,4 @@ std::string ConvertArabic(unsigned int number)
     return romanNumeral;
 }
 
-TEST(ArabicToNumeral, ConversionTestCases)
-{
-    assertThat(1).isConvertedToRomanNumeral("I");
-    assertThat(2).isConvertedToRomanNumeral("II");
-    assertThat(3).isConvertedToRomanNumeral("III");
-    assertThat(10).isConvertedToRomanNumeral("X");
-    assertThat(20).isConvertedToRomanNumeral("XX");
-    assertThat(30).isConvertedToRomanNumeral("XXX");
-    assertThat(33).isConvertedToRomanNumeral("XXXIII");
-    assertThat(100).isConvertedToRomanNumeral("C");
-    assertThat(200).isConvertedToRomanNumeral("CC");
-    assertThat(300).isConvertedToRomanNumeral("CCC");
-    assertThat(500).isConvertedToRomanNumeral("D");
-    assertThat(1000).isConvertedToRomanNumeral("M");
-    assertThat(2000).isConvertedToRomanNumeral("MM");
-    assertThat(3000).isConvertedToRomanNumeral("MMM");
-    assertThat(3333).isConvertedToRomanNumeral("MMMCCCXXXIII");
-    assertThat(3999).isConvertedToRomanNumeral("MMMCMXCIX");
-    assertThat(5).isConvertedToRomanNumeral("V");
-    assertThat(6).isConvertedToRomanNumeral("VI");
-    assertThat(4).isConvertedToRomanNumeral("IV");
 
-}
